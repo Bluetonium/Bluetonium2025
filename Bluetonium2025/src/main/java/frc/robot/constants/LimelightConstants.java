38cf6f41373e9d579b8ac422851b6037a82b33df
Wildbush <75509166+Wildbush76@users.noreply.github.com>
2025-02-06 18:35:52 -0600

    fixing limlieght issues

--- Bluetonium2025/src/main/java/frc/robot/constants/LimelightConstants.java
+++ Bluetonium2025/src/main/java/frc/robot/constants/LimelightConstants.java
@@ -3,6 +3,6 @@ package frc.robot.constants;
 import frc.utils.LimelightConfigs;
 
 public class LimelightConstants {
-    public static final LimelightConfigs MAIN_LL = new LimelightConfigs("", 
-    0.0, 0.0, 0.1398, 0.0, 0.0, 180.0);
+    public static final LimelightConfigs MAIN_LL = new LimelightConfigs("",
+            0.375, 0.0, 0.135, 0.0, 0.0, 180.0);
 }
