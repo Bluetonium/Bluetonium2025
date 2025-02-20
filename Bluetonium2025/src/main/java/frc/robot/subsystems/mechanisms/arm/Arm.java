aa3f858bb6d0829bd3d769bf2cef43c9e726dbdf
ok yay <72277679+ok-yay@users.noreply.github.com>
2025-02-20 08:20:12 -0600

    start on the sim

--- Bluetonium2025/src/main/java/frc/robot/subsystems/mechanisms/arm/Arm.java
+++ Bluetonium2025/src/main/java/frc/robot/subsystems/mechanisms/arm/Arm.java
@@ -1,10 +1,17 @@
 package frc.robot.subsystems.mechanisms.arm;
 
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.networktables.NTSendable;
 import edu.wpi.first.networktables.NTSendableBuilder;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.simulation.BatterySim;
+import edu.wpi.first.wpilibj.simulation.PWMSim;
+import edu.wpi.first.wpilibj.simulation.RoboRioSim;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
+import com.revrobotics.sim.SparkMaxSim;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
@@ -14,7 +21,8 @@ import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 public class Arm implements Subsystem, NTSendable {
-    
+    private SingleJointedArmSim armSim;
+    private SparkMaxSim motorSim;
     private SparkMax arm;
     private SparkMaxConfig armConfig;
     private SparkClosedLoopController armPID;
@@ -35,12 +43,24 @@ public class Arm implements Subsystem, NTSendable {
             .d(ArmConstants.kD);
             armConfig.idleMode(ArmConstants.ARM_MOTOR_IDLE_MODE);
         arm.configure(armConfig,ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-
+        // it goes num of motors, gear ratio, moment of inertia(lol)
+        armSim = new SingleJointedArmSim(DCMotor.getNeo550(1), 0, 0, 0, 0, 90, false, 0, null);
+        motorSim = new SparkMaxSim(arm, DCMotor.getNeo550(1));
     }
 
     @Override
     public void initSendable(NTSendableBuilder builder) {
-        
+    }
+
+    @Override
+    public void simulationPeriodic() { // man idfk
+        armSim.setInput(motorSim.getVelocity() * RobotController.getBatteryVoltage());
+
+        armSim.update(0.020);
+
+        RoboRioSim.setVInVoltage(
+        BatterySim.calculateDefaultBatteryLoadedVoltage(armSim.getCurrentDrawAmps()));
+
     }
 
     public Command setArmPosition(double position) {
