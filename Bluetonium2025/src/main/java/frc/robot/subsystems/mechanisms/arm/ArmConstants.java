3a061be4eb0e9b587fff28ee5d2e46eac45af319
Wildbush <75509166+Wildbush76@users.noreply.github.com>
2025-02-21 12:43:20 -0600

    fixing elevator naming

--- Bluetonium2025/src/main/java/frc/robot/subsystems/mechanisms/arm/ArmConstants.java
+++ Bluetonium2025/src/main/java/frc/robot/subsystems/mechanisms/arm/ArmConstants.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems.mechanisms.arm;
 
 import com.ctre.phoenix6.signals.NeutralModeValue;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import frc.utils.sim.ArmConfig;
 
@@ -9,21 +8,19 @@ public class ArmConstants {
     public static final int ARM_MOTOR_CAN_ID = 20; // we GOTTA figure it out!!!
 
     // PID (we'll figure it out eventually!!!)
-    
+
     public static final double kP = 0;
     public static final double kI = 0;
     public static final double kD = 0;
     public static final NeutralModeValue ARM_MOTOR_NEUTRAL_MODE = NeutralModeValue.Brake; // find out once more :)
 
     public static final ArmConfig SIM_CONFIG = new ArmConfig( // TODO: change this shiet
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1
-    );
-
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1);
 
 }
