d6e9d22fbf64b4e2063ab9c9d5a61ab4743f2736
Wildbush <75509166+Wildbush76@users.noreply.github.com>
2025-02-21 23:06:58 -0600

    adding the new control structure

--- Bluetonium2025/src/main/java/frc/robot/subsystems/driver/Drivers.java
+++ Bluetonium2025/src/main/java/frc/robot/subsystems/driver/Drivers.java
@@ -1,9 +1,6 @@
 package frc.robot.subsystems.driver;
 
-import java.lang.reflect.Field;
 import java.util.function.DoubleSupplier;
-
-import edu.wpi.first.wpilibj.DriverStation;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -12,27 +9,30 @@ import lombok.Setter;
 public class Drivers {
     public static class DriverConfigs {
         @Setter
-        private int port = 0;
+        protected int port = 0;
         @Setter
-        private boolean elevatorControl = false;
+        protected boolean elevatorControl = false;
         @Setter
-        private boolean chassisDriving = false;
+        protected boolean chassisDriving = false;
     }
 
-    // Controllers
-    private final XboxController driver1Controller;
-    private final XboxController driver2Controller;
+    // Controller
+    private final XboxController controller;
 
     // Control axis
-    public DoubleSupplier chassisControlTranslation;
-    public DoubleSupplier chassisControlStrafe;
-    public DoubleSupplier chassisControlRotation;
+    public static DoubleSupplier chassisControlTranslation;
+    public static DoubleSupplier chassisControlStrafe;
+    public static DoubleSupplier chassisControlRotation;
 
     // Triggers
+    // Other
+    private Trigger function;
+    private Trigger noFunction;
+
     // Chassis
-    public Trigger wheelsXPosition;
-    public Trigger steerWheels;
-    public Trigger zeroHeading;
+    public static Trigger wheelsXPosition;
+    public static Trigger steerWheels;
+    public static Trigger zeroHeading;
 
     // Elevator
     public static Trigger L1;
@@ -40,38 +40,18 @@ public class Drivers {
     public static Trigger L3;
     public static Trigger L4;
 
-    /***
-     * Checks to see if two configurations share any controls and throw an error if
-     * they do
-     */
-    private void checkOverlap(DriverConfigs config1, DriverConfigs config2) {
-        Field[] fields = DriverConfigs.class.getDeclaredFields();
-        for (Field field : fields) {
-            try {
-                if (field.getType() == boolean.class && field.getBoolean(config1) == true
-                        && field.getBoolean(config2) == true) {
+    private void applyConfigs(DriverConfigs configs) {
+        function = new Trigger(controller::getStartButton);
+        noFunction = function.not();
 
-                    throw new RuntimeException();
-                }
-            } catch (IllegalAccessException access) {
-                DriverStation.reportWarning("Failed to check overlap on driver controlls", access.getStackTrace());
-            } catch (RuntimeException config) {
-                DriverStation.reportError("Driver Control Overlap - %s".formatted(field.getName()),
-                        config.getStackTrace());
-            }
-        }
-
-    }
-
-    private void applyConfigs(DriverConfigs configs, XboxController controller) {
         if (configs.chassisDriving) {
             chassisControlTranslation = controller::getLeftY;
             chassisControlStrafe = controller::getLeftX;
             chassisControlRotation = controller::getRightX;
 
-            wheelsXPosition = new Trigger(controller::getXButton);
-            steerWheels = new Trigger(controller::getAButton);
-            zeroHeading = new Trigger(controller::getLeftBumperButton);
+            wheelsXPosition = new Trigger(controller::getXButton).and(noFunction);
+            steerWheels = new Trigger(controller::getAButton).and(noFunction);
+            zeroHeading = new Trigger(controller::getLeftBumperButton).and(noFunction);
 
         }
 
@@ -81,19 +61,15 @@ public class Drivers {
             L2 = new Trigger(() -> controller.getPOV() == 90);
             L3 = new Trigger(() -> controller.getPOV() == 180);
             L4 = new Trigger(() -> controller.getPOV() == 270);
-
         }
 
     }
 
-    public Drivers(DriverConfigs driver1Configs, DriverConfigs driver2Configs) {
-        driver1Controller = new XboxController(driver1Configs.port);
-        driver2Controller = new XboxController(driver2Configs.port);
-        checkOverlap(driver1Configs, driver2Configs);
+    public Drivers(DriverConfigs driverConfigs) {
+        controller = new XboxController(driverConfigs.port);
 
         /** setting Configs */
-        applyConfigs(driver1Configs, driver1Controller);
-        applyConfigs(driver2Configs, driver2Controller);
+        applyConfigs(driverConfigs);
 
         /* Triggers */
 
