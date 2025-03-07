a77373d4527591cf1585234a2b1e869594543a1d
ok yay <72277679+ok-yay@users.noreply.github.com>
2025-03-06 19:13:43 -0600

    Update TunerConstants.java

--- Bluetonium2025/src/main/java/frc/robot/subsystems/mechanisms/swerve/TunerConstants.java
+++ Bluetonium2025/src/main/java/frc/robot/subsystems/mechanisms/swerve/TunerConstants.java
@@ -76,7 +76,7 @@ public class TunerConstants {
 
         // CAN bus that the devices are located on;
         // All swerve devices must share the same CAN bus
-        public static final CANBus kCANBus = new CANBus("", "./logs/example.hoot");
+        public static final CANBus kCANBus = new CANBus("CANTivore", "./logs/example.hoot");
 
         // Theoretical free speed (m/s) at 12 V applied output;
         // This needs to be tuned to your individual robot
@@ -94,7 +94,7 @@ public class TunerConstants {
         private static final boolean kInvertLeftSide = false;
         private static final boolean kInvertRightSide = true;
 
-        private static final int kPigeonId = 14;
+        private static final int kPigeonId = 15;
 
         // These are only used for simulation
         private static final MomentOfInertia kSteerInertia = KilogramSquareMeters.of(0.01);
