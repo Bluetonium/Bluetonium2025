268461c5558eee3739d5402eabdaf08575b4f7c9
Wildbush <75509166+Wildbush76@users.noreply.github.com>
2025-03-17 11:27:50 -0500

    revising limelights

--- Bluetonium2025/src/main/java/frc/robot/subsystems/limelight/LimelightConstants.java
+++ Bluetonium2025/src/main/java/frc/robot/subsystems/limelight/LimelightConstants.java
@@ -1,9 +1,20 @@
 package frc.robot.subsystems.limelight;
 
-
-import frc.utils.LimelightConfigs;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
 
 public class LimelightConstants {
-    public static final LimelightConfigs MAIN_LL = new LimelightConfigs("",
-            0.3175, -0.0127, 0.8382, -30.0, 0.0, 0.0);
+    public static final LimelightConfig MAIN_LL = new LimelightConfig("",
+            new Translation3d(0.3175, -0.0127, 0.8382),
+            new Rotation3d(0, 0. - 30.0, 0.0));
+
+    public static enum Pipelines {
+        LOCALIZATION(0);
+
+        public final int pipeline;
+
+        private Pipelines(int pipeline) {
+            this.pipeline = pipeline;
+        }
+    }
 }
---
+++ Bluetonium2025/src/main/java/frc/robot/subsystems/limelight/Limelights.java
@@ -0,0 +1,75 @@
+package frc.robot.subsystems.limelight;
+
+import com.ctre.phoenix6.hardware.Pigeon2;
+
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.limelight.LimelightConstants.Pipelines;
+import frc.robot.subsystems.mechanisms.swerve.CommandSwerveDrivetrain;
+import frc.utils.LimelightHelpers;
+
+public class Limelights extends SubsystemBase {
+    private static CommandSwerveDrivetrain drivetrain;
+    private LimelightConfig[] limelights;
+    private Pigeon2 gyro;
+
+    /***
+     * 
+     * @param drivetrain the drive train - needed so we can add vision measurements
+     * @param limelights names of the limelights that will be used
+     */
+    public Limelights(LimelightConfig... limelights) {
+        limelights = new LimelightConfig[limelights.length];
+        for (int i = 0; i < limelights.length; i++) {
+            addLimelight(limelights[i], i);
+        }
+    }
+
+    private void addLimelight(LimelightConfig limelight, int index) {
+        LimelightHelpers.setCameraPose_RobotSpace(limelight.name, limelight.position.getX(), limelight.position.getY(),
+                limelight.position.getZ(), limelight.rotation.getX(), limelight.rotation.getY(),
+                limelight.rotation.getZ());
+        limelights[index] = limelight;
+    }
+
+    public void setup() {
+        drivetrain = RobotContainer.getSwerve();
+        gyro = drivetrain.getPigeon2();
+        drivetrain.setVisionMeasurementStdDevs(VecBuilder.fill(.7, .7, Integer.MAX_VALUE));
+        setDefaultCommand(configureForLocalization());
+    }
+
+    private Command configureForLocalization() {
+        return runOnce(() -> {
+            for (LimelightConfig limelight : limelights) {
+                LimelightHelpers.setPipelineIndex(limelight.name, Pipelines.LOCALIZATION.pipeline);
+            }
+        });
+
+    }
+
+    @Override
+    public void periodic() {
+        for (LimelightConfig limelight : limelights) {
+            if (LimelightHelpers.getCurrentPipelineIndex(limelight.name) != Pipelines.LOCALIZATION.pipeline)
+                continue;
+
+            LimelightHelpers.SetRobotOrientation(limelight.name, drivetrain.getState().Pose.getRotation().getDegrees(),
+                    gyro.getAngularVelocityZWorld().getValueAsDouble(), 0, 0, 0, 0);
+
+            LimelightHelpers.PoseEstimate estimatedPosition = LimelightHelpers
+                    .getBotPoseEstimate_wpiBlue_MegaTag2(limelight.name);
+            if (estimatedPosition == null || estimatedPosition.tagCount == 0
+                    || Math.abs(gyro.getAngularVelocityZWorld().getValueAsDouble()) > 720)
+                continue;// reject measurements if not seeing a tag or going too fast
+
+            double timeStamp = drivetrain.getState().Timestamp - estimatedPosition.latency / 1000;
+            drivetrain.setVisionMeasurementStdDevs(VecBuilder.fill(.7, .7, Integer.MAX_VALUE));
+            drivetrain.addVisionMeasurement(estimatedPosition.pose,
+                    timeStamp);
+
+        }
+    }
+}
