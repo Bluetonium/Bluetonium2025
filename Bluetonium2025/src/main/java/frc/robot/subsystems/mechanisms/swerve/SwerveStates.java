4061845ea61d92d1ea6e0ff69516b827b219c705
Wildbush <75509166+Wildbush76@users.noreply.github.com>
2025-03-26 22:38:32 -0500

    removing music playing trigger

--- Bluetonium2025/src/main/java/frc/robot/subsystems/mechanisms/swerve/SwerveStates.java
+++ Bluetonium2025/src/main/java/frc/robot/subsystems/mechanisms/swerve/SwerveStates.java
@@ -1,13 +1,9 @@
 package frc.robot.subsystems.mechanisms.swerve;
 
-import java.io.File;
-
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+
 import frc.robot.RobotContainer;
 import frc.robot.RobotStates;
 import frc.robot.subsystems.driver.Drivers;
@@ -29,23 +25,6 @@ public class SwerveStates {
         RobotStates.reefAlignLeft.whileTrue(swerve.AlignToReefRegion(true));
         RobotStates.reefAlignRight.whileTrue(swerve.AlignToReefRegion(false));
         RobotStates.coralStationAlign.whileTrue(swerve.AlignToCoralStation());
-
-        SendableChooser<String> musicChooser = new SendableChooser<>();
-
-        File directory = new File("Music/");
-        File[] filesArray = directory.listFiles();
-        if (filesArray != null) {
-            for (File file : filesArray) {
-                if (file.isFile()) {
-                    musicChooser.addOption(file.getName(), file.getName());
-                }
-            }
-        }
-        SmartDashboard.putData("Music Choice", musicChooser);
-
-        SmartDashboard.putBoolean("Respectful", false);
-        new Trigger(() -> SmartDashboard.getBoolean("Respectful", false))
-                .whileTrue(swerve.playMusic("Music/undertale.chrp"));
     }
 
 }
