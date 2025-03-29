99d7e02541ba828ffdfa42aa5c80b153699ed716
Wildbush <75509166+Wildbush76@users.noreply.github.com>
2025-03-28 21:57:25 -0500

    setting the limelight positions

--- Bluetonium2025/src/main/java/frc/robot/subsystems/limelight/LimelightConstants.java
+++ Bluetonium2025/src/main/java/frc/robot/subsystems/limelight/LimelightConstants.java
@@ -2,14 +2,23 @@ package frc.robot.subsystems.limelight;
 
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
 
 public class LimelightConstants {
-    public static final LimelightConfig MAIN_LL = new LimelightConfig("",
-            new Translation3d(0.3175, -0.0127, 0.8382),
-            new Rotation3d(0, 0. - 30.0, 0.0));
+    public static final LimelightConfig UPPER_LL = new LimelightConfig("upper",
+            new Translation3d(Units.inchesToMeters(3.5), Units.inchesToMeters(-2), Units.inchesToMeters(35.5)),
+            new Rotation3d(0, Math.toRadians(10), 0.0));
+    public static final LimelightConfig LOWER_LEFT_LL = new LimelightConfig("lowerleft",
+            new Translation3d(Units.inchesToMeters(8), Units.inchesToMeters(10.5), Units.inchesToMeters(10.5)),
+            new Rotation3d(0, 10, Math.toRadians(-20)));
+    public static final LimelightConfig LOWER_RIGHT_LL = new LimelightConfig("lowerright",
+            new Translation3d(Units.inchesToMeters(8), Units.inchesToMeters(-10.5), Units.inchesToMeters(10.5)),
+            new Rotation3d(0, 10, Math.toRadians(20)));
 
     public static enum Pipelines {
-        LOCALIZATION(0);
+        LOCALIZATION(0),
+        REEF_TARGETING(1),
+        CORAL_STATION_TARGETING(2);
 
         public final int pipeline;
 
